5944dcbdeb16c87d33bab3da8b2e870269563e97
sohaila38 <101292660+sohaila38@users.noreply.github.com>
2023-11-08 08:53:36 +0200

    Add files via upload

---
+++ TUIO/TUIO_test/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace TUIO_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(494, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 299);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ TUIO/TuioDemo.cs
@@ -0,0 +1,343 @@
+
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using TUIO;
+
+	public class TuioDemo : Form , TuioListener
+	{
+		private TuioClient client;
+		private Dictionary<long,TuioObject> objectList;
+		private Dictionary<long,TuioCursor> cursorList;
+		private Dictionary<long,TuioBlob> blobList;
+
+		public static int width, height;
+		private int window_width =  640;
+		private int window_height = 480;
+		private int window_left = 0;
+		private int window_top = 0;
+		private int screen_width = Screen.PrimaryScreen.Bounds.Width;
+		private int screen_height = Screen.PrimaryScreen.Bounds.Height;
+
+		private bool fullscreen;
+		private bool verbose;
+
+		Font font = new Font("Arial", 10.0f);
+		SolidBrush fntBrush = new SolidBrush(Color.White);
+		SolidBrush bgrBrush = new SolidBrush(Color.FromArgb(0,0,64));
+		SolidBrush curBrush = new SolidBrush(Color.FromArgb(192, 0, 192));
+		SolidBrush objBrush = new SolidBrush(Color.FromArgb(64, 0, 0));
+		SolidBrush blbBrush = new SolidBrush(Color.FromArgb(64, 64, 64));
+		Pen curPen = new Pen(new SolidBrush(Color.Blue), 1);
+
+	private Dictionary<long, Image> markerImages = new Dictionary<long, Image>();
+
+	public TuioDemo(int port) {
+		
+			verbose = false;
+			fullscreen = false;
+			width = window_width;
+			height = window_height;
+
+			this.ClientSize = new System.Drawing.Size(width, height);
+			this.Name = "TuioDemo";
+			this.Text = "TuioDemo";
+			
+			this.Closing+=new CancelEventHandler(Form_Closing);
+			this.KeyDown+=new KeyEventHandler(Form_KeyDown);
+
+			this.SetStyle( ControlStyles.AllPaintingInWmPaint |
+							ControlStyles.UserPaint |
+							ControlStyles.DoubleBuffer, true);
+
+			objectList = new Dictionary<long,TuioObject>(128);
+			cursorList = new Dictionary<long,TuioCursor>(128);
+			blobList   = new Dictionary<long,TuioBlob>(128);
+			
+			client = new TuioClient(port);
+			client.addTuioListener(this);
+
+			client.connect();
+		    LoadMarkerImages();
+	}
+	private void LoadMarkerImages()
+	{
+		string markerImagesDirectory = "C:\Users\sohai\OneDrive\Desktop\New folder";
+
+		if (Directory.Exists(markerImagesDirectory))
+		{
+			string[] imageFiles = Directory.GetFiles(markerImagesDirectory);
+
+			foreach (string imageFile in imageFiles)
+			{
+				string imageName = Path.GetFileNameWithoutExtension(imageFile);
+
+				if (long.TryParse(imageName, out long symbolID))
+				{
+					Image image = Image.FromFile(imageFile);
+					markerImages[symbolID] = image;
+				}
+			}
+		}
+	}
+
+		private void Form_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e) {
+
+ 			if ( e.KeyData == Keys.F1) {
+	 			if (fullscreen == false) {
+
+					width = screen_width;
+					height = screen_height;
+
+					window_left = this.Left;
+					window_top = this.Top;
+
+					this.FormBorderStyle = FormBorderStyle.None;
+		 			this.Left = 0;
+		 			this.Top = 0;
+		 			this.Width = screen_width;
+		 			this.Height = screen_height;
+
+		 			fullscreen = true;
+	 			} else {
+
+					width = window_width;
+					height = window_height;
+
+		 			this.FormBorderStyle = FormBorderStyle.Sizable;
+		 			this.Left = window_left;
+		 			this.Top = window_top;
+		 			this.Width = window_width;
+		 			this.Height = window_height;
+
+		 			fullscreen = false;
+	 			}
+ 			} else if ( e.KeyData == Keys.Escape) {
+				this.Close();
+
+ 			} else if ( e.KeyData == Keys.V ) {
+ 				verbose=!verbose;
+ 			}
+
+ 		}
+
+		private void Form_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			client.removeTuioListener(this);
+
+			client.disconnect();
+			System.Environment.Exit(0);
+		}
+
+		public void addTuioObject(TuioObject o)
+	    {
+			lock(objectList)
+		    {
+				objectList.Add(o.SessionID,o);
+
+			// Check if the detected object corresponds to a marker
+			if (markerImages.ContainsKey(o.SymbolID))
+			{
+				DisplayImageForMarker(o.SymbolID);
+			}
+		    } 
+		if (verbose) Console.WriteLine("add obj "+o.SymbolID+" ("+o.SessionID+") "+o.X+" "+o.Y+" "+o.Angle);
+	    }
+	private void DisplayImageForMarker(long symbolID)
+	{
+		if (markerImages.TryGetValue(symbolID, out Image markerImage))
+		{
+			// Display the image in a PictureBox 
+			PictureBox pictureBox = new PictureBox
+			{
+				Image = markerImage,
+				SizeMode = PictureBoxSizeMode.StretchImage,
+				Dock = DockStyle.Fill
+			};
+			pictureBox.Show();
+			
+			// Replace the existing PictureBox or create a new one to display the image
+			// You can add it to the form or a specific container as needed
+			Controls.Add(pictureBox);
+		}
+	}
+
+	public void updateTuioObject(TuioObject o) 
+	    {
+
+			if (verbose) Console.WriteLine("set obj "+o.SymbolID+" "+o.SessionID+" "+o.X+" "+o.Y+" "+o.Angle+" "+o.MotionSpeed+" "+o.RotationSpeed+" "+o.MotionAccel+" "+o.RotationAccel);
+		// Check if the objectList contains the current TuioObject
+		if (objectList.ContainsKey(o.SessionID))
+		{
+			// Get the SymbolID of the TuioObject
+			int symbolID = o.SymbolID;
+
+			// Construct the filename of the image based on SymbolID (e.g., "1.png", "2.png")
+			string imageFileName = Path.Combine("MarkerImages", symbolID + ".png");
+
+			// Check if the image file exists
+			if (File.Exists(imageFileName))
+			{
+				// Load the image
+				using (Image markerImage = Image.FromFile(imageFileName))
+				{
+					// Getting the graphics object
+					using (Graphics g = CreateGraphics())
+					{
+						int ox = o.getScreenX(width);
+						int oy = o.getScreenY(height);
+
+						// Calculate the size of the marker image (you can adjust this based on your needs)
+						int imageSize = height / 5;
+
+						g.DrawImage(markerImage, new Rectangle(ox - imageSize / 2, oy - imageSize / 2, imageSize, imageSize));
+					}
+				}
+			}
+		}
+
+		public void removeTuioObject(TuioObject o) {
+			lock(objectList) {
+				objectList.Remove(o.SessionID);
+			}
+			if (verbose) Console.WriteLine("del obj "+o.SymbolID+" ("+o.SessionID+")");
+		}
+
+		public void addTuioCursor(TuioCursor c) {
+			lock(cursorList) {
+				cursorList.Add(c.SessionID,c);
+			}
+			if (verbose) Console.WriteLine("add cur "+c.CursorID + " ("+c.SessionID+") "+c.X+" "+c.Y);
+		}
+
+		public void updateTuioCursor(TuioCursor c) {
+			if (verbose) Console.WriteLine("set cur "+c.CursorID + " ("+c.SessionID+") "+c.X+" "+c.Y+" "+c.MotionSpeed+" "+c.MotionAccel);
+		}
+
+		public void removeTuioCursor(TuioCursor c) {
+			lock(cursorList) {
+				cursorList.Remove(c.SessionID);
+			}
+			if (verbose) Console.WriteLine("del cur "+c.CursorID + " ("+c.SessionID+")");
+ 		}
+
+		public void addTuioBlob(TuioBlob b) {
+			lock(blobList) {
+				blobList.Add(b.SessionID,b);
+			}
+			if (verbose) Console.WriteLine("add blb "+b.BlobID + " ("+b.SessionID+") "+b.X+" "+b.Y+" "+b.Angle+" "+b.Width+" "+b.Height+" "+b.Area);
+		}
+
+		public void updateTuioBlob(TuioBlob b) {
+		
+			if (verbose) Console.WriteLine("set blb "+b.BlobID + " ("+b.SessionID+") "+b.X+" "+b.Y+" "+b.Angle+" "+b.Width+" "+b.Height+" "+b.Area+" "+b.MotionSpeed+" "+b.RotationSpeed+" "+b.MotionAccel+" "+b.RotationAccel);
+		}
+
+		public void removeTuioBlob(TuioBlob b) {
+			lock(blobList) {
+				blobList.Remove(b.SessionID);
+			}
+			if (verbose) Console.WriteLine("del blb "+b.BlobID + " ("+b.SessionID+")");
+		}
+
+		public void refresh(TuioTime frameTime) {
+			Invalidate();
+		}
+
+		protected override void OnPaintBackground(PaintEventArgs pevent)
+		{
+			// Getting the graphics object
+			Graphics g = pevent.Graphics;
+			g.FillRectangle(bgrBrush, new Rectangle(0,0,width,height));
+
+			// draw the cursor path
+			if (cursorList.Count > 0) {
+ 			 lock(cursorList) {
+			 foreach (TuioCursor tcur in cursorList.Values) {
+					List<TuioPoint> path = tcur.Path;
+					TuioPoint current_point = path[0];
+
+					for (int i = 0; i < path.Count; i++) {
+						TuioPoint next_point = path[i];
+						g.DrawLine(curPen, current_point.getScreenX(width), current_point.getScreenY(height), next_point.getScreenX(width), next_point.getScreenY(height));
+						current_point = next_point;
+					}
+					g.FillEllipse(curBrush, current_point.getScreenX(width) - height / 100, current_point.getScreenY(height) - height / 100, height / 50, height / 50);
+					g.DrawString(tcur.CursorID + "", font, fntBrush, new PointF(tcur.getScreenX(width) - 10, tcur.getScreenY(height) - 10));
+				}
+			}
+		 }
+
+			// draw the objects
+			if (objectList.Count > 0) {
+ 				lock(objectList) {
+					foreach (TuioObject tobj in objectList.Values) {
+						int ox = tobj.getScreenX(width);
+						int oy = tobj.getScreenY(height);
+						int size = height / 10;
+
+						g.TranslateTransform(ox, oy);
+						g.RotateTransform((float)(tobj.Angle / Math.PI * 180.0f));
+						g.TranslateTransform(-ox, -oy);
+
+						g.FillRectangle(objBrush, new Rectangle(ox - size / 2, oy - size / 2, size, size));
+
+						g.TranslateTransform(ox, oy);
+						g.RotateTransform(-1 * (float)(tobj.Angle / Math.PI * 180.0f));
+						g.TranslateTransform(-ox, -oy);
+
+						g.DrawString(tobj.SymbolID + "", font, fntBrush, new PointF(ox - 10, oy - 10));
+					}
+				}
+			}
+
+			// draw the blobs
+			if (blobList.Count > 0) {
+				lock(blobList) {
+					foreach (TuioBlob tblb in blobList.Values) {
+						int bx = tblb.getScreenX(width);
+						int by = tblb.getScreenY(height);
+						float bw = tblb.Width*width;
+						float bh = tblb.Height*height;
+
+						g.TranslateTransform(bx, by);
+						g.RotateTransform((float)(tblb.Angle / Math.PI * 180.0f));
+						g.TranslateTransform(-bx, -by);
+
+						g.FillEllipse(blbBrush, bx - bw / 2, by - bh / 2, bw, bh);
+
+						g.TranslateTransform(bx, by);
+						g.RotateTransform(-1 * (float)(tblb.Angle / Math.PI * 180.0f));
+						g.TranslateTransform(-bx, -by);
+						
+						g.DrawString(tblb.BlobID + "", font, fntBrush, new PointF(bx, by));
+					}
+				}
+			}
+		}
+
+		public static void Main(String[] argv) {
+	 		int port = 0;
+			switch (argv.Length) {
+				case 1:
+					port = int.Parse(argv[0],null);
+					if(port==0) goto default;
+					break;
+				case 0:
+					port = 3333;
+					break;
+				default:
+					Console.WriteLine("usage: mono TuioDemo [port]");
+					System.Environment.Exit(0);
+					break;
+			}
+			
+			TuioDemo app = new TuioDemo(port);
+			Application.Run(app);
+		}
+	}
